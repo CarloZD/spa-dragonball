0775549a794f8cb01a4f1cb9f5a2900dee6b4c9e
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-12-03 16:06:40 -0500

    Confuguracion inicial para la aplicacion, App, main y store pobladas,

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+export const useStore = create((set)=> ({
+    characters: [],
+    loading: false,
+    error: null,
+
+  fetchCharacters: async () => {
+    try {
+      set({ loading: true })
+
+      const res = await fetch('https://dragonball-api.com/api/characters?limit=60')
+      const data = await res.json()
+
+      set({ characters: data.items, loading: false })
+    } catch (err) {
+      set({ error: 'Error al cargar personajes', loading: false })
+    }
+  }
+}))
