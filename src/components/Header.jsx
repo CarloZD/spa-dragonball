0b44b30e0edfa2f7fb8bfee457127e1b544b0b77
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-12-03 16:08:16 -0500

    Header agregado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from 'react-router-dom'
 
-export default function Header(){
-    return (<></>
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
+      <div className="container">
+        <Link className="navbar-brand fw-bold text-dark" to="/">DRAGON BALL SPA</Link>
 
-    )
+        <div className="collapse navbar-collapse show">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link text-dark" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-dark" to="/entities">Personajes</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link text-dark" to="/contact">Contacto</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
